24b4df0053b374ac025fb0cc5cf760c1d310003d
Abhishek Kumar <abhi_jimmy@hotmail.com>
2024-09-26 14:38:25 +0530

    fixed issue related to wrong pickle file

--- streamlit_regression.py
+++ streamlit_regression.py
@@ -9,13 +9,13 @@ import pickle
 model = tf.keras.models.load_model('regression_model.h5')
 
 ## load the encoder and scaler
-with open('onehot_encoder_geo.pkl', 'rb') as file:
+with open('onehot_encoder_geo_r.pkl', 'rb') as file:
     label_encoder_geo = pickle.load(file)
 
-with open('label_encoder_gender.pkl', 'rb') as file:
+with open('label_encoder_gender_r.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
-with open('scaler.pkl', 'rb') as file:
+with open('scaler_r.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 
